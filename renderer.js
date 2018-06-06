a04b98d0d05308230aed974f3344365f5f286cfc
Adrian Darian <adarian@ucmerced.edu>
2018-06-05 19:37:53 -0700

    notifications and input box for messages

--- renderer.js
+++ renderer.js
@@ -1,3 +1,32 @@
+var $ = global.jquery = require('jquery');
+var {ipcRenderer} = require('electron');
+
+
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+$("#sendMessageToMainBtn").click(function() {
+  sendMessageToMainBtn();
+});
+
+function sendMessageToMainBtn() {
+  console.log('renderer_sendMessageToMain');
+  const message = $("#sendBox").val();
+
+  console.log('sending...');
+  console.log(message);
+
+  ipcRenderer.send('key_on_message', {
+    message: message,
+    comingFrom: 'sendMessageToMainBtn',
+  });
+
+  new Notification('Title', {
+    body: 'Here I am'
+  });
+}
+
+ipcRenderer.on('info', (event, arg) => {
+  console.log('renderer_ipcRenderer');
+  console.log(arg);
+})
